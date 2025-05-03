b88b1816f7315742cf9d9cd9986f19f4ed893b92
alejobern4 <alejobernalsan4444@gmail.com>
2025-05-03 18:10:28 -0500

    Create DetalleVehiculo.php
    Con sus respectivas relaciones, sin ningun metodo en especifico solo relaciones

---
+++ app/Models/DetalleVehiculo.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleVehiculo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'detalle_vehiculos';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'id_vehiculo',
+        'user_id',
+        'id_estado',
+        'id_estadoregistro',
+        'placa',
+        'conductor_auxiliar',
+        'fecha_solicitud',
+        'fecha_devolucion',
+        'fecha_soat',
+        'fecha_tecnomecanica',
+        'imagen_vehiculo',
+    ];
+
+    public function vehiculo()
+    {
+        return $this->belongsTo(Vehiculo::class, 'id_vehiculo');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class, 'id_estado');
+    }
+
+    public function estadoRegistro()
+    {
+        return $this->belongsTo(EstadoRegistro::class, 'id_estadoregistro');
+    }
+
+    public function mantenimientos()
+    {
+        return $this->hasMany(Mantenimiento::class, 'id_detallevehiculo');
+    }
+
+    public function reportes()
+    {
+        return $this->hasMany(Reporte::class, 'id_detallevehiculo');
+    }
+
+    public function alertas()
+    {
+        return $this->hasMany(Alerta::class, 'id_detallevehiculo');
+    }
+}
