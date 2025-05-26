3dfde8890dd15f96ed31d2f29c12c3d418892783
alejobern4 <alejobernalsan4444@gmail.com>
2025-05-25 22:11:01 -0500

    Blades Roles
    Pruebas de roles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,19 +16,23 @@
 <div class="dashboard-page">
     <header class="dashboard-header">
         <div class="logo-container">
-            <img src="{{ asset('img/logo_epc.webp') }}" alt="Logo EPC" class="logo" />
-            <span class="user-name">Bienvenido, {{ Auth::user()->name }}</span>
+            {{-- <img src="{{ asset('img/logo_epc.webp') }}" alt="Logo EPC" class="logo" /> --}}
+            <span class="user-name">Bienvenido, {{ Auth::user()->persona->primer_nombre }} {{ Auth::user()->persona->primer_apellido }}</span>
         </div>
-        {{-- Puedes añadir aquí un botón de logout si quieres --}}
-        {{--
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
         <a href="{{ route('logout') }}"
            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
            class="btn btn-secondary">Cerrar Sesión</a>
-        --}}
     </header>
+
+    @auth
+        @if (auth()->user()->tieneRol('Administrador'))
+            <p>Bienvenido, Admin</p>
+        @endif
+    @endauth
+
 
     <div class="dashboard-buttons">
         <a href="#" class="dashboard-button">Vehículos</a>
