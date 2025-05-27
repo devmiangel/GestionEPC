a4afe602f2f16dfdc84c71b4aea0446cf18b1173
JuanVillarraga <jpvillarraga@ucundinamarca.edu.co>
2025-05-26 20:48:21 -0500

    Modificacion dashboard
    Se cambia el estilo y los botones de la pagina principal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,36 +7,90 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard EPC</title>
+    
     <link rel="stylesheet" href="{{ asset('styles/estiloDashboard.css') }}">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </head>
 
 <div class="dashboard-page">
-
-
-<h1 style="text-align: center; margin-top: 20px;">Bienvenido</h1><br>
+    <h1 style="text-align: center; margin-top: 20px; font-size: 50px;"><strong>Bienvenido</strong></h1><br><br>
     <div class="dashboard-buttons">
         <div>
             <a href="#" class="dashboard-button">
-            Vehículos
+                Vehículos
             </a>
-                    @auth
-        @if (auth()->user()->tieneRol('Administrador'))
-            <a href="#" class="dashboard-button">
-                Modificar Vehículos
-            </a>
-        @endif
-    @endauth
+            @auth
+                @if (auth()->user()->tieneRol('Coordinador'))
+                    <a href="#" class="dashboard-buttondos">
+                        
+                        Asignar
+                    </a>
+                @endif
+            @endauth
+            @auth
+                    @if (auth()->user()->tieneRol('Administrador'))
+                    <a href="#" class="dashboard-buttondos">
+                        Modificar
+                    </a>
+                    <a href="#" class="dashboard-buttondos">
+                        
+                        Asignar
+                    </a>
+                @endif
+            @endauth
         </div>
-        
 
-        <a href="#" class="dashboard-button">
-            Herramientas
-        </a>
-        <a href="#" class="dashboard-button">
-            Conductores
-        </a>
+        <div>
+            <a href="#" class="dashboard-button">
+                Herramientas
+            </a>
+            @auth
+                @if (auth()->user()->tieneRol('Coordinador'))
+                    <a href="#" class="dashboard-buttondos">
+                        
+                        Asignar
+                    </a>
+                @endif
+            @endauth
+            @auth
+                    @if (auth()->user()->tieneRol('Administrador'))
+                    <a href="#" class="dashboard-buttondos">
+                        Modificar
+                    </a>
+                    <a href="#" class="dashboard-buttondos">
+                        
+                        Asignar
+                    </a>
+                @endif
+            @endauth
+        </div>
+
+        <div>
+            <a href="#" class="dashboard-button">
+                Conductores
+            </a>
+            @auth
+                @if (auth()->user()->tieneRol('Coordinador'))
+                    <a href="#" class="dashboard-buttondos">
+                        
+                        Asignar
+                    </a>
+                @endif
+            @endauth
+            @auth
+                    @if (auth()->user()->tieneRol('Administrador'))
+                    <a href="#" class="dashboard-buttondos">
+                        Modificar
+                    </a>
+                    <a href="#" class="dashboard-buttondos">
+                        
+                        Asignar
+                    </a>
+                @endif
+            @endauth
+        </div>
     </div>
 </div>
+<br>
 @endsection
