b2e928a4c875fa35a3f4836dd24a530a3a281ef4
alejobern4 <alejobernalsan4444@gmail.com>
2025-05-27 14:01:43 -0500

    Update VehiculoController.php

--- app/Http/Controllers/Modulos/VehiculoController.php
+++ app/Http/Controllers/Modulos/VehiculoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Modulos;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Vehiculo;
 
 class VehiculoController extends Controller
 {
@@ -17,4 +18,42 @@ class VehiculoController extends Controller
     {
         return view('modulos.vehiculos.agregar');
     }
+
+    public function eliminate()
+    {
+        return view('modulos.vehiculos.eliminar');
+    }
+
+    public function camiones()
+    {
+        $camiones = Vehiculo::with('detalleVehiculo')
+                        ->where('id_tipovehiculo', 1)
+                        ->get();
+        return view('modulos.vehiculos.tipos.camiones', compact('camiones'));
+    }
+
+    public function compactadores()
+    {
+        $compactadores = Vehiculo::with('detalleVehiculo')
+                        ->where('id_tipovehiculo', 2)
+                        ->get();
+        return view('modulos.vehiculos.tipos.compactadores', compact('compactadores'));
+    }
+
+    public function motos()
+    {
+        $motos = Vehiculo::with('detalleVehiculo')
+                        ->where('id_tipovehiculo', 3)
+                        ->get();
+        return view('modulos.vehiculos.tipos.motos', compact('motos'));
+    }
+
+    public function otros()
+    {
+        $otros = Vehiculo::with('detalleVehiculo')
+                        ->where('id_tipovehiculo', 4)
+                        ->get();
+        return view('modulos.vehiculos.tipos.otros', compact('otros'));
+    }
+
 }
