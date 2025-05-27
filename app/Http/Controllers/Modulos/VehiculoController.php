a49d64eee4228a98f6732601ba88870bdbc3b40c
alejobern4 <alejobernalsan4444@gmail.com>
2025-05-26 21:16:13 -0500

    Create VehiculoController.php
    Controlador de la vistas de vehiculos

---
+++ app/Http/Controllers/Modulos/VehiculoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Modulos;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VehiculoController extends Controller
+{
+    public function index()
+    {
+        // Aquí podrías obtener datos desde base de datos si deseas.
+        return view('modulos.vehiculos.index');
+    }
+}
