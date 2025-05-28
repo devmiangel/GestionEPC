2d6ee838a1662077a4593eedcf2de34407342537
alejobern4 <alejobernalsan4444@gmail.com>
2025-05-28 01:27:34 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             TipoDocumentoSeeder::class,
             EstadoRegistroSeeder::class,
             TiposVehiculosSeeder::class,
+            Estadoseeder::class,
         ]);
     }
 }
