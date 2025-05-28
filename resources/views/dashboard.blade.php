1574a300450bc8bdb14c859cc10cb3a2790333a0
JMR564 <juanmanuelra5@gmail.com>
2025-05-27 21:52:34 -0500

    correcion footer y formulario

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,18 +3,9 @@
 @section('title', 'Dashboard - EPC')
 
 @section('content')
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dashboard EPC</title>
-    
-    <link rel="stylesheet" href="{{ asset('styles/estiloDashboard.css') }}">
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-</head>
-
 <div class="dashboard-page">
     <h1 style="text-align: center; margin-top: 20px; font-size: 50px;"><strong>Bienvenido</strong></h1><br><br>
+
     <div class="dashboard-buttons">
         <div>
             <a href="{{ route('vehiculos.index') }}" class="dashboard-button">
@@ -22,46 +13,26 @@
             </a>
             @auth
                 @if (auth()->user()->tieneRol('Coordinador'))
-                    <a href="#" class="dashboard-buttondos">
-                        
-                        Asignar
-                    </a>
+                    <a href="#" class="dashboard-buttondos">Asignar</a>
                 @endif
-            @endauth
-            @auth
-                    @if (auth()->user()->tieneRol('Administrador'))
-                    <a href="#" class="dashboard-buttondos">
-                        Modificar
-                    </a>
-                    <a href="#" class="dashboard-buttondos">
-                        
-                        Asignar
-                    </a>
+                @if (auth()->user()->tieneRol('Administrador'))
+                    <a href="#" class="dashboard-buttondos">Modificar</a>
+                    <a href="#" class="dashboard-buttondos">Asignar</a>
                 @endif
             @endauth
         </div>
 
         <div>
-            <a href="{{ route('herramientas.index') }}" class="dashboard-button">
+            <a href="#" class="dashboard-button">
                 Herramientas
             </a>
             @auth
                 @if (auth()->user()->tieneRol('Coordinador'))
-                    <a href="#" class="dashboard-buttondos">
-                        
-                        Asignar
-                    </a>
+                    <a href="#" class="dashboard-buttondos">Asignar</a>
                 @endif
-            @endauth
-            @auth
-                    @if (auth()->user()->tieneRol('Administrador'))
-                    <a href="#" class="dashboard-buttondos">
-                        Modificar
-                    </a>
-                    <a href="#" class="dashboard-buttondos">
-                        
-                        Asignar
-                    </a>
+                @if (auth()->user()->tieneRol('Administrador'))
+                    <a href="#" class="dashboard-buttondos">Modificar</a>
+                    <a href="#" class="dashboard-buttondos">Asignar</a>
                 @endif
             @endauth
         </div>
@@ -72,25 +43,23 @@
             </a>
             @auth
                 @if (auth()->user()->tieneRol('Coordinador'))
-                    <a href="#" class="dashboard-buttondos">
-                        
-                        Asignar
-                    </a>
+                    <a href="#" class="dashboard-buttondos">Asignar</a>
                 @endif
-            @endauth
-            @auth
-                    @if (auth()->user()->tieneRol('Administrador'))
-                    <a href="#" class="dashboard-buttondos">
-                        Modificar
-                    </a>
-                    <a href="#" class="dashboard-buttondos">
-                        
-                        Asignar
-                    </a>
+                @if (auth()->user()->tieneRol('Administrador'))
+                    <a href="#" class="dashboard-buttondos">Modificar</a>
+                    <a href="#" class="dashboard-buttondos">Asignar</a>
                 @endif
             @endauth
         </div>
     </div>
 </div>
-<br>
 @endsection
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('styles/estiloDashboard.css') }}">
+@endpush
+
+@push('scripts')
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+@endpush
--- resources/views/layouts/vehiculos.blade.php
+++ resources/views/layouts/vehiculos.blade.php
@@ -4,28 +4,28 @@
     <link rel="stylesheet" href="{{ asset('styles/vehiculo.css') }}">
     <link rel="stylesheet" href="{{ asset('styles/estiloDashboard.css') }}">
 @endsection
+
 @section('content')
+<main class="main-content">
+    <div class="vehicle-grid">
+        <div class="vehicle-card">
+            {{-- Contenido de vehículos --}}
+        </div>
+    </div>
 
-<div class="vehicle-grid">
-    <div class="vehicle-card">
-        @yield('content')
+    <div class="vehicle-category">
+        <div class="cuadroVehiculos">
+            <div class="actions-vehiculos">
+                <div class="action-buttons">
+                    <a href="agregarvehiculo.html" class="btn-agregar-vehiculo">
+                        <i class="fas fa-plus"></i> Añadir Vehículo
+                    </a>
+                    <a href="eliminarvehiculo.html" class="btn-eliminar-vehiculo">
+                        <i class="fas fa-trash-alt"></i> Eliminar Vehículo
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-
-<div class="vehicle-category">
-    <div class="cuadroVehiculos">
-      <div class="actions-vehiculos">
-    <div class="action-buttons">
-        <a href="agregarvehiculo.html" class="btn-agregar-vehiculo">
-            <i class="fas fa-plus"></i> Añadir Vehículo
-        </a>
-        <a href="eliminarvehiculo.html" class="btn-eliminar-vehiculo">
-            <i class="fas fa-trash-alt"></i> Eliminar Vehículo
-        </a>
-    </div>
-</div>
-    </div>
-</div>
-<script src="{{ asset('js/modulos.js') }}"></script>
-<script src="{{ asset('js/vehiculos.js') }}"></script>
+</main>
 @endsection
